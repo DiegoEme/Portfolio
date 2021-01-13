b7e1352756341f8b7e20372a14b6af4a98b2e5cb
Diego <soterg.uhdeia@gmail.com>
2021-01-12 22:28:34 -0500

    added projects

--- portafolio.js
+++ portafolio.js
@@ -10,3 +10,31 @@ navLinks.forEach((link) => {
     document.body.classList.remove("nav-open");
   });
 });
+
+const cards = document.querySelectorAll(".flip-card");
+
+const grid = document.getElementById("grid");
+grid.addEventListener("click", cartas);
+
+function cartas(e) {
+  let carta = e.target;
+
+  if (!carta.matches("img")) {
+    return;
+  }
+
+  if (carta.classList.contains("imagen")) {
+    imagenes.push(carta.alt);
+    console.log("carta", imagenes);
+  }
+}
+
+cards.forEach((card) => card.addEventListener("click", flipCard));
+
+function flipCard() {
+  if (this.classList.contains("flip")) {
+    this.classList.remove("flip");
+  } else {
+    this.classList.add("flip");
+  }
+}
